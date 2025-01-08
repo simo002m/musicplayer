496efca8048bfdb427961a761cb5ae9700c9dfcb
uzair <uzair@live.dk>
2025-01-08 12:35:03 +0100

    lol

--- src/main/java/com/musicplayer/HelloController.java
+++ src/main/java/com/musicplayer/HelloController.java
@@ -4,22 +4,54 @@ import Services.SongDAO;
 import Services.SongDAOImpl;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
+    // This method will be triggered when the user clicks the "Choose File" button
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void chooseFile() {
+        // Create a new FileChooser instance
+        FileChooser fileChooser = new FileChooser();
+
+        // Set up the file filters for MP3 and WAV files
+        FileChooser.ExtensionFilter mp3Filter = new FileChooser.ExtensionFilter("MP3 Files", "*.mp3");
+        FileChooser.ExtensionFilter wavFilter = new FileChooser.ExtensionFilter("WAV Files", "*.wav");
+
+        // Add the filters to the file chooser
+        fileChooser.getExtensionFilters().add(mp3Filter);
+        fileChooser.getExtensionFilters().add(wavFilter);
+
+        // Open the file chooser and get the selected file
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        // If a file is selected, do something with the file (e.g., display the file name)
+        if (selectedFile != null) {
+            // Here you can use the file for further processing, such as playing it
+            welcomeText.setText("Selected file: " + selectedFile.getName());
+
+            // Example: you can add functionality to play the music using MediaPlayer
+            // For instance, create a MediaPlayer here to play the song.
+        } else {
+            welcomeText.setText("No file selected");
+        }
     }
 
+    // Initialize method to load all songs (you already have this method)
     public void initialize() {
         SongDAO songdao = new SongDAOImpl();
 
         try {
             songdao.getAllSongs();
         } catch (Exception e) {
+            // Handle any exceptions here, if needed
+            e.printStackTrace();
         }
     }
 }
